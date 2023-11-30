90ab3e2cfe76363b09ed0d84c77a8f28e86ae0f8
Mazes2 <136519686+Mazes2@users.noreply.github.com>
2023-11-30 09:39:59 -0300

    CadastroDAO.java

--- Projeto Final/CadastroDAO.java
+++ Projeto Final/CadastroDAO.java
@@ -33,4 +33,57 @@ public class CadastroDAO {
             e.printStackTrace();
         }
     }
+
+    public static List<Pessoa> consultarPessoas() {
+        List<Pessoa> pessoas = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(URL);
+             Statement statement = conn.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM pessoas");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                String endereco = resultSet.getString("endereco");
+                String cpf = resultSet.getString("cpf");
+                String cargo = resultSet.getString("cargo");
+
+                if (cpf != null) {
+                    pessoas.add(new Cliente(id, nome, endereco, cpf));
+                } else {
+                    pessoas.add(new Funcionario(id, nome, endereco, cargo));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pessoas;
+    }
+
+    public static void atualizarPessoa(int id, Pessoa pessoa) {
+        try (Connection conn = DriverManager.getConnection(URL);
+             PreparedStatement statement = conn.prepareStatement("UPDATE pessoas SET nome=?, endereco=?, cpf=?, cargo=? WHERE id=?")) {
+            statement.setString(1, pessoa.getNome());
+            statement.setString(2, pessoa.getEndereco());
+            if (pessoa instanceof Cliente) {
+                statement.setString(3, ((Cliente) pessoa).getCpf());
+                statement.setString(4, null); // Set cargo to null for generic update
+            } else {
+                statement.setString(3, null); // Set cpf to null for generic update
+                statement.setString(4, ((Funcionario) pessoa).getCargo());
+            }
+            statement.setInt(5, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void excluirPessoa(int id) {
+        try (Connection conn = DriverManager.getConnection(URL);
+             PreparedStatement statement = conn.prepareStatement("DELETE FROM pessoas WHERE id=?")) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
